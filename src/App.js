1a8bf539db473e5f52834a4026f0f8071e984ab8
janhenrikfock <jan.henrik.fock@googlemail.com>
2021-02-02 08:05:23 +0100

    add-object button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from './actions/index'
 
 function App() {
+  const [array, setArray] = useState([])
   const counter = useSelector((state) => state.counter)
   const dispatch = useDispatch()
-  console.log()
+
+  function addObject() {
+    const additionalObject = {
+      name: 'value',
+    }
+    setArray([...array, additionalObject])
+  }
+
   return (
     <div className="App">
       <h1>Counter {counter} </h1>
-      <button onClick={() => dispatch(increment(2))}>+</button>
-      <button onClick={() => dispatch(decrement(2))}>-</button>
+      <button onClick={() => dispatch(increment(1))}>+</button>
+      <button onClick={() => dispatch(decrement(1))}>-</button>
+      <h2>Arrays</h2>
+      <button onClick={addObject}>AddObject {array.length}</button>
     </div>
   )
 }
